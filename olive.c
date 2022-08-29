b12efe82d94af4f3279156558e2136057aa37798
AbdelrahmanHafez <Dahomy999@outlook.sa>
2022-08-29 12:05:36 +0200

    Bresenham integer line algorithm

--- olive.c
+++ olive.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <errno.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 typedef int Errno;
 
@@ -138,40 +140,59 @@ void olivec_fill_circle(uint32_t *pixels, size_t pixels_width, size_t pixels_hei
     }
 }
 
+void olivec_draw_pixel(uint32_t *pixels, size_t pixels_width, size_t pixels_height, int x, int y, uint32_t color)
+{
+    if (x < 0 || x >= (int)pixels_width || y < 0 || y >= (int)pixels_height)
+        return;
+
+    pixels[y * pixels_width + x] = color;
+}
+
 // TODO: lines with different thicness
 void olivec_draw_line(uint32_t *pixels, size_t pixels_width, size_t pixels_height,
                       int x1, int y1, int x2, int y2,
                       uint32_t color)
 {
-    // TODO: fix the olivec_draw_line stairs
+    bool steep = abs(y2 - y1) > abs(x2 - x1);
+
+    if (steep)
+    {
+        OLIVEC_SWAP(int, x1, y1);
+        OLIVEC_SWAP(int, x2, y2);
+    }
+
+    if (x1 > x2)
+    {
+        OLIVEC_SWAP(int, x1, x2);
+        OLIVEC_SWAP(int, y1, y2);
+    }
+
+    int deltay;
+    if (y1 > y2)
+        deltay = -1;
+    else
+        deltay = 1;
+
     int dx = x2 - x1;
-    int dy = y2 - y1;
+    int dy = abs(y2 - y1);
+    int y = y1;
+    int error = 0;
 
-    if (dx != 0) {
-        int c = y1 - dy*x1/dx;
+    for (int x = x1; x < x2; x++)
+    {
+        if (x < 0 || x >= (int)pixels_width)
+            continue;
 
-        if (x1 > x2) OLIVEC_SWAP(int, x1, x2);
-        for (int x = x1; x <= x2; ++x) {
-            if (0 <= x && x < (int) pixels_width) {
-                int sy1 = dy*x/dx + c;
-                int sy2 = dy*(x + 1)/dx + c;
-                if (sy1 > sy2) OLIVEC_SWAP(int, sy1, sy2);
-                for (int y = sy1; y <= sy2; ++y) {
-                    if (0 <= y && y < (int) pixels_height) {
-                        pixels[y*pixels_width + x] = color;
-                    }
-                }
-            }
-        }
-    } else {
-        int x = x1;
-        if (0 <= x && x < (int) pixels_width) {
-            if (y1 > y2) OLIVEC_SWAP(int, y1, y2);
-            for (int y = y1; y <= y2; ++y) {
-                if (0 <= y && y < (int) pixels_height) {
-                    pixels[y*pixels_width + x] = color;
-                }
-            }
+        if (steep)
+            olivec_draw_pixel(pixels, pixels_width, pixels_height, y, x, color);
+        else
+            olivec_draw_pixel(pixels, pixels_width, pixels_height, x, y, color);
+
+        error += dy;
+        if (2 * error >= dx)
+        {
+            y = y + deltay;
+            error -= dx;
         }
     }
 }
