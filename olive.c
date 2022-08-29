b6eddc99f5187d933e83114d7cf75ba1c5715703
AbdelrahmanHafez <Dahomy999@outlook.sa>
2022-08-29 12:03:42 +0200

    Auto Formatting

--- olive.c
+++ olive.c
@@ -7,14 +7,26 @@
 
 typedef int Errno;
 
-#define return_defer(value) do { result = (value); goto defer; } while (0)
-#define OLIVEC_SWAP(T, a, b) do { T t = a; a = b; b = t; } while (0)
+#define return_defer(value) \
+    do                      \
+    {                       \
+        result = (value);   \
+        goto defer;         \
+    } while (0)
+#define OLIVEC_SWAP(T, a, b) \
+    do                       \
+    {                        \
+        T t = a;             \
+        a = b;               \
+        b = t;               \
+    } while (0)
 #define OLIVEC_SIGN(T, x) ((T)((x) > 0) - (T)((x) < 0))
-#define OLIVEC_ABS(T, x) (OLIVEC_SIGN(T, x)*(x))
+#define OLIVEC_ABS(T, x) (OLIVEC_SIGN(T, x) * (x))
 
 void olivec_fill(uint32_t *pixels, size_t width, size_t height, uint32_t color)
 {
-    for (size_t i = 0; i < width*height; ++i) {
+    for (size_t i = 0; i < width * height; ++i)
+    {
         pixels[i] = color;
     }
 }
@@ -26,25 +38,30 @@ Errno olivec_save_to_ppm_file(uint32_t *pixels, size_t width, size_t height, con
 
     {
         f = fopen(file_path, "wb");
-        if (f == NULL) return_defer(errno);
+        if (f == NULL)
+            return_defer(errno);
 
         fprintf(f, "P6\n%zu %zu 255\n", width, height);
-        if (ferror(f)) return_defer(errno);
+        if (ferror(f))
+            return_defer(errno);
 
-        for (size_t i = 0; i < width*height; ++i) {
+        for (size_t i = 0; i < width * height; ++i)
+        {
             uint32_t pixel = pixels[i];
             uint8_t bytes[3] = {
-                (pixel>>(8*0))&0xFF,
-                (pixel>>(8*1))&0xFF,
-                (pixel>>(8*2))&0xFF,
+                (pixel >> (8 * 0)) & 0xFF,
+                (pixel >> (8 * 1)) & 0xFF,
+                (pixel >> (8 * 2)) & 0xFF,
             };
             fwrite(bytes, sizeof(bytes), 1, f);
-            if (ferror(f)) return_defer(errno);
+            if (ferror(f))
+                return_defer(errno);
         }
     }
 
 defer:
-    if (f) fclose(f);
+    if (f)
+        fclose(f);
     return result;
 }
 
@@ -56,17 +73,23 @@ void olivec_fill_rect(uint32_t *pixels, size_t pixels_width, size_t pixels_heigh
                       int x1, int y1, int w, int h,
                       uint32_t color)
 {
-    int x2 = x1 + OLIVEC_SIGN(int, w)*(OLIVEC_ABS(int, w) - 1);
-    if (x1 > x2) OLIVEC_SWAP(int, x1, x2);
-    int y2 = y1 + OLIVEC_SIGN(int, h)*(OLIVEC_ABS(int, h) - 1);
-    if (y1 > y2) OLIVEC_SWAP(int, y1, y2);
+    int x2 = x1 + OLIVEC_SIGN(int, w) * (OLIVEC_ABS(int, w) - 1);
+    if (x1 > x2)
+        OLIVEC_SWAP(int, x1, x2);
+    int y2 = y1 + OLIVEC_SIGN(int, h) * (OLIVEC_ABS(int, h) - 1);
+    if (y1 > y2)
+        OLIVEC_SWAP(int, y1, y2);
 
-    for (int y = y1; y <= y2; ++y) {
+    for (int y = y1; y <= y2; ++y)
+    {
         // TODO: move boundary checks out of the loops in olivec_fill_rect
-        if (0 <= y && y < (int) pixels_height) {
-            for (int x = x1; x <= x2; ++x) {
-                if (0 <= x && x < (int) pixels_width) {
-                    pixels[y*pixels_width + x] = color;
+        if (0 <= y && y < (int)pixels_height)
+        {
+            for (int x = x1; x <= x2; ++x)
+            {
+                if (0 <= x && x < (int)pixels_width)
+                {
+                    pixels[y * pixels_width + x] = color;
                 }
             }
         }
@@ -81,25 +104,33 @@ void olivec_fill_circle(uint32_t *pixels, size_t pixels_width, size_t pixels_hei
                         int cx, int cy, int r,
                         uint32_t color)
 {
-    if (r == 0) return;
+    if (r == 0)
+        return;
 
     int x1 = cx - r;
     int x2 = cx + r;
-    if (x1 > x2) OLIVEC_SWAP(int, x1, x2);
+    if (x1 > x2)
+        OLIVEC_SWAP(int, x1, x2);
 
     int y1 = cy - r;
     int y2 = cy + r;
-    if (y1 > y2) OLIVEC_SWAP(int, y1, y2);
+    if (y1 > y2)
+        OLIVEC_SWAP(int, y1, y2);
 
-    for (int y = y1; y <= y2; ++y) {
+    for (int y = y1; y <= y2; ++y)
+    {
         // TODO: move boundary checks out of the loops in olivec_fill_circle
-        if (0 <= y && y < (int) pixels_height) {
-            for (int x = x1; x <= x2; ++x) {
-                if (0 <= x && x < (int) pixels_width) {
+        if (0 <= y && y < (int)pixels_height)
+        {
+            for (int x = x1; x <= x2; ++x)
+            {
+                if (0 <= x && x < (int)pixels_width)
+                {
                     int dx = x - cx;
                     int dy = y - cy;
-                    if (dx*dx + dy*dy <= r*r) {
-                        pixels[y*pixels_width + x] = color;
+                    if (dx * dx + dy * dy <= r * r)
+                    {
+                        pixels[y * pixels_width + x] = color;
                     }
                 }
             }
