e468a5c59bc0de4cd3020fac100969b5508d8101
rexim <reximkut@gmail.com>
2022-08-30 00:24:59 +0700

    Remove dead code

--- olive.c
+++ olive.c
@@ -5,8 +5,6 @@
 #include <stdint.h>
 #include <errno.h>
 
-typedef int Errno;
-
 #define OLIVEC_SWAP(T, a, b) do { T t = a; a = b; b = t; } while (0)
 #define OLIVEC_SIGN(T, x) ((T)((x) > 0) - (T)((x) < 0))
 #define OLIVEC_ABS(T, x) (OLIVEC_SIGN(T, x)*(x))
